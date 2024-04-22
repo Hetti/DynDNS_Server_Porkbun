d35a252171a597d6b3f9137af22c0e3c1295b5a7
Hetti <git@thepeko.at>
2024-04-23 01:40:54 +0200

    fix(dyndns): error handling

--- dyndns.py
+++ dyndns.py
@@ -25,28 +25,26 @@ class PorkbunDNSAPIClient:
         self.secretapikey = config["secretapikey"]
         self.default_ttl = config["default_ttl"]
 
-        self.api_header = {
-            "secretapikey": self.secretapikey,
-            "apikey": self.apikey
-        }
-
     def getRootDomain(self, full_domain: str) -> str:
         root_domain = '.'.join(full_domain.split('.')[-2:])
-        print(root_domain)
         return root_domain
 
     def getSubDomain(self, full_domain: str) -> str:
         sub_domain = '.'.join(full_domain.split('.')[:-2])
-        print(sub_domain)
         return sub_domain
 
 
     def getRecords(self, domain: str): #grab all the records so we know which ones to delete to make room for our record. Also checks to make sure we've got the right domain
-        print(f"checking {domain}")
-        allRecords=json.loads(requests.post(self.base_url + '/dns/retrieve/' + domain, data = json.dumps(self.api_header)).text)
+        api_header = {
+            "secretapikey": self.secretapikey,
+            "apikey": self.apikey
+        }
+        
+        print(f"Getting records for {domain}")
+        allRecords = json.loads(requests.post(self.base_url + '/dns/retrieve/' + domain, data = json.dumps(api_header)).text)
         if allRecords["status"]=="ERROR":
             print('Error getting domain. Check to make sure you specified the correct domain, and that API access has been switched on for this domain.');
-            sys.exit();
+            sys.exit()
         return(allRecords)
 
     def updateRecord(self, records: str, record_type: str, record_name: str, root_domain: str,  record_data: str, record_ttl: str):
@@ -61,92 +59,21 @@ class PorkbunDNSAPIClient:
 
         for i in records["records"]:
             if i["name"]==record_name and (i["type"] == record_type):
-                print(f"Editing " + i["type"] + " record of {record_name}")
-                deleteRecord = json.loads(requests.post(self.base_url + '/dns/edit/' + root_domain + '/' + i["id"], data = json.dumps(api_header)).text)
+                print(f"Editing {i['type']} record of {record_name}")
+                updateRecord = json.loads(requests.post(self.base_url + '/dns/edit/' + root_domain + '/' + i["id"], data = json.dumps(api_header)).text)
 
+        if updateRecord["status"] == "ERROR":
+            raise DNSAPIException(500, updateRecord['message'])
 
-    def api_request(self, endpoint: str, payload: dict):
-        response = requests.post(f"{self.base_url}/{endpoint}", json=payload)
-
-        if response.status_code != 200:
-            try:
-                error_text = response.json()
-            except ValueError:
-                error_text = response.content.decode("utf-8")
-
-            raise DNSAPIException(response.status_code, error_text)
-
-        json_data = response.json()
-
-        # hosting.de return 200 on failed transactions, very nice! /s
-        # Probably because errors and successes can be mixed.
-        # We only do one transaction at a time, so it's either an error or a success.
-        if "errors" in json_data and len(json_data["errors"]):
-            error_data = json_data["errors"][0]
-            if error_data.get("value"):
-                error_text = f"{error_data['value']} - {error_data['text']}"
-            else:
-                error_text = error_data['text']
-
-            raise DNSAPIException(error_data["code"], error_text)
-
-        return response.json()["response"]
-
-        if len(result["data"]) == 0:
-            raise DNSAPIException(404, "No matching record found.")
-        elif len(result["data"]) > 1:
-            raise DNSAPIException(400, "More than one matching record found, cannot continue.")
-        elif not result["data"][0].get("id"):
-            raise DNSAPIException(404, "Could not find ID for record in response.")
-
-        return result["data"][0]["id"]
+        return(updateRecord)
 
     def update_resource_record(self, record_name: str, record_type: str, record_data: str, record_ttl: int = None):
-
-        print(record_name)
+        record_ttl = record_ttl if record_ttl and record_ttl >= 60 else self.default_ttl
         root_domain = self.getRootDomain(record_name)
-        print(self.getSubDomain(record_name))
         records = self.getRecords(root_domain)
         result = self.updateRecord(records,record_type, record_name, root_domain, record_data, record_ttl)
 
-        # FIX return value check
-
-        # result = self.api_request(
-        #     "recordsUpdate",
-        #     {
-        #         "authToken": self.token,
-        #         "zoneName": self.zone,
-        #         "recordsToModify": [
-        #             {
-        #                 "id": record_id,
-        #                 "name": record_name,
-        #                 "type": record_type,
-        #                 "content": record_data,
-        #                 "comments": "DynDNS Record - automatically managed, do not change!",
-        #                 # Minimum TTL for hosting.de is 60s
-        #                 "ttl": record_ttl if record_ttl and record_ttl >= 60 else self.default_ttl
-        #             }
-        #         ]
-        #     }
-        # )
-
-        # filtered = list(filter(lambda entry: entry.get("id") == record_id, result.get("records", [])))
-        # if len(filtered) == 0:
-        #     raise DNSAPIException(
-        #         500,
-        #         "Update succeeded, but failed to find updated record in success response. This should not happen."
-        #     )
-        # elif len(filtered) > 1:
-        #     raise DNSAPIException(
-        #         500,
-        #         "Update succeeded, but found more than one result in success response. This should not happen."
-        #     )
-
-        # fix returned results
-        # return filtered[0]
-
-        # fix return value
-        return "YOLO"
+        return result
 
 
 class Root:
@@ -174,10 +101,10 @@ class Root:
             try:
                 result = self.dns_client.update_resource_record(domain, record_type, record_data, ttl)
                 logging.info(
-                    f"Successfully updated {record_type} record for {result['name']}:"
-                    f" {result['content']}, TTL {result['ttl']} at {result['lastChangeDate']}"
+                    f"Successfully updated {record_type} record for {domain}:"
+                    f" {record_data}, TTL {ttl if ttl else '<default>'}"
                 )
-                results.append(f"Successfully updated {record_type} record for {result['name']} to {result['content']}")
+                results.append(f"Successfully updated {record_type} record for {domain} to {record_data}")
             except DNSAPIException as e:
                 logging.error(
                     f"Failed to update {record_type} record for {domain}"
